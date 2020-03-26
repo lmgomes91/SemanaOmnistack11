c154ddfe15d07324f60ece102674831e5c19823b
LucasGomes <lgomes@post.com>
2020-03-26 02:49:59 -0300

    frontEnd criado

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './global.css'
+
+import Routes from './routes'
+//import { Route } from 'react-router-dom';
+
+function App() {
+  return (
+   <Routes />
+  );
+}
+
+export default App;
